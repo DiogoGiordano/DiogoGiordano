9cfe021aaf2a4628444ddd5ccf700f1e2f38a0ad
DiogoGio <89301560+DiogoGiordano@users.noreply.github.com>
2021-12-08 21:41:54 -0300

    Add files via upload
    Classes: Cliente, ClientePF, ClientePJ, Conta, ContaCorrente, ContaEspecial, ContaPoupanca, Main.

---
+++ POO/src/com/company/ContaPoupanca.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class ContaPoupanca extends ContaCorrente {
+
+
+    public ContaPoupanca(String nome, Cliente cliente, int senha) {
+        super(nome, cliente, senha);
+    }
+
+
+
+}
+
---
+++ POO/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        /*Cliente c1 = new Cliente("Rodrigo");
+        System.out.println(c1.toString());
+
+        ClientePF cPF = new ClientePF("Giovana", 12345, 123145);
+        System.out.println(cPF.toString());
+
+        ClientePJ cPJ = new ClientePJ("Rubia", 54321);
+        System.out.println(cPJ.toString());
+
+        Conta conta1 = new Conta(c1, 1111);
+        System.out.println(conta1.toString());
+        System.out.println(c1.toString());
+        System.out.println(conta1.getSaldo());
+        conta1.deposito(200.00);
+        System.out.println(conta1.getSaldo());
+        conta1.saque(20.00);
+        System.out.println(conta1.getSaldo());
+        conta1.encerraConta();
+        System.out.println(conta1.toString());
+        System.out.println(c1.toString());
+
+        Conta conta2 = new Conta(cPF, 2222);
+        System.out.println(conta2.toString());
+        System.out.println(cPF.toString());
+
+        Conta conta3 = new Conta(cPJ, 3333);
+        System.out.println(conta3.toString());
+        System.out.println(cPJ.toString());*/
+
+    }
+
+}
